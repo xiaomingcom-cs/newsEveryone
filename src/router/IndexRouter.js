2536e809aeda5dd6257f2d40f8903b97a163edcd
Xiaoming <xiaomingwangmm@gmail.com>
2023-04-24 21:31:00 +0800

    完成游客访问新闻子系统

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Login from '../views/login/Login'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
+import News from '../views/news/News'
+import Detail from '../views/news/Detail'
 
 export default function IndexRouter() {
   return (
 
     <Switch>
       <Route path='/login' component={Login} />
+      <Route path='/news' component={News} />
+      <Route path='/detail/:id' component={Detail} /> 
       <Route path='/' render={() => 
         localStorage.getItem('token') ?
           <NewsSandBox></NewsSandBox> :
